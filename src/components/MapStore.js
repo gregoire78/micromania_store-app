fc31aa37f51570ac4d56de57d37755934ddd5d33
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2019-07-16 18:23:38 +0200

    cluster leaflet

--- src/components/MapStore.js
+++ src/components/MapStore.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import { Icon, LatLngBounds, LatLng, divIcon, Point } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { Icon, LatLngBounds, LatLng, divIcon, Point, marker, popup } from 'leaflet';
 import { Map, TileLayer, Marker, Popup, ZoomControl, Tooltip, Polyline, GeoJSON, ScaleControl, MapLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 export default function MapStore(props) {
+    const url = "https://wxs.ign.fr/wi5arjlfu40r0fbm6vw8nzny/geoportail/wmts?" +
+        "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
+        "&STYLE=normal" +
+        "&TILEMATRIXSET=PM" +
+        "&FORMAT=image/jpeg" +
+        "&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGN" +
+        "&TILEMATRIX={z}" +
+        "&TILEROW={y}" +
+        "&TILECOL={x}"
     return (
-        <Map center={[51.505, -0.09]} zoom={13} style={{ width: '100%', height: '100%', zIndex: "1" }}>
+        <Map center={[46.52863469527167, 2.43896484375]} zoom={6} style={{ width: '100%', height: 'calc(100vh - 50px)', zIndex: "1" }}>
             <ZoomControl position="bottomleft" />
             <ScaleControl position="bottomleft" />
             <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                tileSize={256}
+                minZoom={0}
+                maxZoom={18}
+                attribution='IGN-F/Geoportail'
+                url={url}
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-            <GeoJSON data={props.geojson} />
+            <MarkerClusterGroup>
+                {props.geojson.features.length > 0 && <GeoJSON data={props.geojson} pointToLayer={(geoJsonPoint, latlng) => {
+                    console.log(geoJsonPoint)
+                    //return popup().setLatLng(latlng).setContent(geoJsonPoint.properties.description)
+                    return marker(latlng, {title: geoJsonPoint.properties.description});
+                }} />}
+            </MarkerClusterGroup>
         </Map>
     );
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Map from '../components/MapStore';
 
 export default function Home() {
+
+    const [geoJson, setGeoJson] = useState({
+        "type": "FeatureCollection",
+        "features": []
+    });
+
+    useEffect(() => {
+        async function getGeoJson() {
+            const response = await fetch(`http://localhost:3000/v1/geojson`, {
+                method: 'GET'
+            });
+            const data = await response.json();
+            setGeoJson(data)
+            return data.data;
+        }
+        getGeoJson()
+    }, [])
     return (
         <main>
-            <Map geojson={""} />
+            <Map geojson={geoJson} />
         </main>
     )
 }
