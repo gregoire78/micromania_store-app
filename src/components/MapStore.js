c77e5f64d9c3bbf0ec97d88ea41878fe9a3840a2
Grégoire Joncour <greg.joncour@gmail.com>
2019-07-18 00:06:00 +0200

    map clusters custom

--- src/components/MapStore.js
+++ src/components/MapStore.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup, ZoomControl, Tooltip, Polyline, GeoJSON, ScaleControl, MapLayer } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON, ScaleControl } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 export default function MapStore(props) {
     const map = useRef(null);
-    const url = "https://wxs.ign.fr/wi5arjlfu40r0fbm6vw8nzny/geoportail/wmts?" +
-        "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
-        "&STYLE=normal" +
-        "&TILEMATRIXSET=PM" +
-        "&FORMAT=image/jpeg" +
-        "&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGN" +
-        "&TILEMATRIX={z}" +
-        "&TILEROW={y}" +
-        "&TILECOL={x}"
     return (
         <Map ref={map} center={[46.52863469527167, 2.43896484375]} zoom={6} style={{ width: '100%', height: 'calc(100vh - 50px)', zIndex: "1" }}>
             <ScaleControl position="bottomleft" />
             <TileLayer
                 tileSize={256}
-                minZoom={0}
-                maxZoom={18}
-                attribution='IGN-F/Geoportail'
-                url={url}
+                minZoom={1}
+                maxZoom={20}
+                attribution='donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>'
+                url='//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
             />
-            <MarkerClusterGroup>
+            <MarkerClusterGroup
+            iconCreateFunction={(cluster) => {
+                return L.divIcon({
+                  html: `<span>${cluster.getChildCount()}</span>`,
+                  className: 'marker-cluster-custom',
+                  iconSize: L.point(30, 30, true),
+                });
+              }}
+            >
                 {props.geojson.features.length > 0 && <GeoJSON data={props.geojson} pointToLayer={(geoJsonPoint, latlng) => {
                     return L.marker(latlng, {
                         title: "", icon: L.icon({
